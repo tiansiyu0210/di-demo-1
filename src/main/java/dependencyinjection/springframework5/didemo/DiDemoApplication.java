5b46c8c44751ed5a5fda2e82cc739d9cfda8509f
tiansiyu0210 <tiansiyu297@gmail.com>
2018-06-24 20:14:54 -0400

    L-35 primary annotation

---
+++ src/main/java/dependencyinjection/springframework5/didemo/DiDemoApplication.java
@@ -0,0 +1,27 @@
+package dependencyinjection.springframework5.didemo;
+
+import dependencyinjection.springframework5.didemo.controller.ConstructorInjectedController;
+import dependencyinjection.springframework5.didemo.controller.MyController;
+import dependencyinjection.springframework5.didemo.controller.PropertyInjectedController;
+import dependencyinjection.springframework5.didemo.controller.SetterInjectedController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = SpringApplication.run(DiDemoApplication.class, args);
+
+        MyController myController = (MyController) applicationContext.getBean("myController");
+
+        //myController.hello();
+
+        System.out.println(myController.hello());
+        System.out.println(applicationContext.getBean(PropertyInjectedController.class).sayHello());
+        System.out.println(applicationContext.getBean(SetterInjectedController.class).sayHello());
+        System.out.println(applicationContext.getBean(ConstructorInjectedController.class).sayHello());
+    }
+}
